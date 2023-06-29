f2dbf7d9fc9e5ee62adf63022bf90c0dd423bd66
RAHUL <rahulmeetawa@gmail.com>
2023-06-29 10:28:30 +0530

    add a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <h1 className="m-3 text-4xl text-green-500 animate-bounce font-bold ">
+      Chuck Norries
+    </h1>
+  );
+};
+
+export default Header;
